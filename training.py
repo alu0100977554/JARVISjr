220cb12b6b0d4d67612c1bc6e933c5c0297d12a7
Pablo Gil de San Nicolás <alu0100977554@ull.edu.es>
2022-01-24 17:01:28 +0000

    Valores numéricos para "patterns"
    Se han asignado valores iniciales a cada palabra en words, con los que la red neuronal trabajará para obtener una respuesta adecuada

--- training.py
+++ training.py
@@ -1,4 +1,6 @@
 import json
+import random
+import numpy as np
 
 import nltk                                 #natural language tool kit
 from nltk.stem import WordNetLemmatizer     # Herramienta para identificar variaciones de una palabra
@@ -24,3 +26,26 @@ for intent in intents['intents']:
 words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
 words = sorted(set(words))                              # Eliminar posibles duplicados
 classes = sorted(set(classes))
+
+# El siguiente paso consiste en asignar valores numéricos a cada palabra,
+# para que la red neuronal pueda realizar las operaciones correspondientes
+
+training = []
+output_empty = [0] * len(classes)
+
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1          
+    training.append([bag, output_row])
+
+random.shuffle(training)
+training = np.array(training)
+
+training_x = list(training[:, 0])
+training_y = list(training[:, 1])
